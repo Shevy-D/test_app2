fc552c6146b3abe3d85e7fd5906e584f9a016c51
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-02-20 12:31:17 +0500

    Add check password

---
+++ app/src/main/java/com/example/test_app/Constance.kt
@@ -0,0 +1,15 @@
+package com.example.test_app
+
+object Constance {
+    const val DIRECTOR_SALARY = 100000
+    const val ENGINEER_SALARY = 50000
+    const val CLEANER_SALARY = 10000
+
+    const val DIRECTOR_PASSWORD = "4565"
+    const val ENGINEER_PASSWORD = "1235"
+    const val CLEANER_PASSWORD = "7766"
+
+    const val DIRECTOR = "Egor"
+    const val ENGINEER = "Andrey"
+    const val CLEANER = "Sergey"
+}
--- app/src/main/java/com/example/test_app/MainActivity.kt
+++ app/src/main/java/com/example/test_app/MainActivity.kt
@@ -19,22 +19,54 @@ class MainActivity : AppCompatActivity() {
         setContentView(bindingClass.root)
 
         bindingClass.btResult.setOnClickListener {
-            val resultValue: Int = bindingClass.edValue.text.toString().toInt()
+            Log.d("testlog", "ID picture = ${R.drawable.ic_launcher_background}")
+            val resultValue = bindingClass.edValue.text.toString()
+            bindingClass.imPhoto.visibility = View.VISIBLE
             Log.d("testlog", "Result = $resultValue")
             when(resultValue){
-                in 0..1000 -> {
+                Constance.DIRECTOR -> {
                     bindingClass.tvResult.visibility = View.VISIBLE
-                    bindingClass.tvResult.text = "Вы начинающий блогер"
+                    val tempText = "Получите ваши ${Constance.DIRECTOR_SALARY}"
+                    if (bindingClass.edPassword.text.toString() == Constance.DIRECTOR_PASSWORD){
+                        bindingClass.tvResult.text = tempText
+                        bindingClass.imPhoto.setImageResource(R.drawable.first)
+                    } else {
+                        bindingClass.imPhoto.visibility = View.VISIBLE
+                        bindingClass.tvResult.text = "Неверный пароль"
+                        bindingClass.imPhoto.setImageResource(R.drawable.ic_launcher_background)
+                    }
                 }
 
-                in 1000..100000 -> {
+                Constance.ENGINEER -> {
                     bindingClass.tvResult.visibility = View.VISIBLE
-                    bindingClass.tvResult.text = "Вы средний блогер"
+                    val tempText = "Получите ваши ${Constance.ENGINEER_SALARY}"
+                    if (bindingClass.edPassword.text.toString() == Constance.ENGINEER_PASSWORD){
+                        bindingClass.tvResult.text = tempText
+                        bindingClass.imPhoto.setImageResource(R.drawable.second)
+                    } else {
+                        bindingClass.imPhoto.visibility = View.VISIBLE
+                        bindingClass.tvResult.text = "Неверный пароль"
+                        bindingClass.imPhoto.setImageResource(R.drawable.ic_launcher_background)
+                    }
                 }
 
+                Constance.CLEANER -> {
+                    bindingClass.tvResult.visibility = View.VISIBLE
+                    val tempText = "Получите ваши ${Constance.CLEANER_SALARY}"
+                    if (bindingClass.edPassword.text.toString() == Constance.CLEANER_PASSWORD){
+                        bindingClass.tvResult.text = tempText
+                        bindingClass.imPhoto.setImageResource(R.drawable.third)
+                    } else {
+                        bindingClass.imPhoto.visibility = View.VISIBLE
+                        bindingClass.tvResult.text = "Неверный пароль"
+                        bindingClass.imPhoto.setImageResource(R.drawable.ic_launcher_background)
+                    }
+                }
                 else -> {
+                    bindingClass.imPhoto.visibility = View.VISIBLE
                     bindingClass.tvResult.visibility = View.VISIBLE
-                    bindingClass.tvResult.text = "Вы суперзвезда"
+                    bindingClass.tvResult.text = "Нет такого работника"
+                    bindingClass.imPhoto.setImageResource(R.drawable.ic_launcher_background)
                 }
             }
         }
