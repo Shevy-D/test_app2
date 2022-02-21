86d7ae91dfe16eca33f0cea44fa2e87aa03ca5d9
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-02-21 22:29:52 +0500

    Add sing in & sing up

--- app/src/main/java/com/example/test_app/MainActivity.kt
+++ app/src/main/java/com/example/test_app/MainActivity.kt
@@ -1,40 +1,88 @@
 package com.example.test_app
 
 import android.content.Intent
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import androidx.core.view.isVisible
+import com.example.test_app.constance.Constance
 import com.example.test_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var bindingClass: ActivityMainBinding
+    private var login: String = "empty"
+    private var password: String = "empty"
+    private var name: String = "empty"
+    private var lastName: String = "empty"
+    private var secondName: String = "empty"
+    private var avatarImageId: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
-
-        val message = intent.getStringExtra("key")
-        bindingClass.tvMessageMain.text = message
-
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 100 && resultCode == RESULT_OK && data != null){
-            bindingClass.tvMessageMain.text = data.getStringExtra("key2")
+
+        if (requestCode == Constance.REQUEST_CODE_SIGN_IN){
+
+            val l = data?.getStringExtra(Constance.LOGIN)
+            val p = data?.getStringExtra(Constance.PASSWORD)
+            if (login == l && password == p){
+
+                bindingClass.imAvatar.visibility = View.VISIBLE
+                bindingClass.imAvatar.setImageResource(avatarImageId)
+                val textInfo = "$name $lastName $secondName"
+                bindingClass.tvInfo.text = textInfo
+                bindingClass.bHide.visibility = View.GONE
+                bindingClass.bExit.text = "Выйти"
+            } else {
+                bindingClass.imAvatar.visibility = View.VISIBLE
+                bindingClass.imAvatar.setImageResource(R.drawable.ic_launcher_background)
+                bindingClass.tvInfo.text = "Такого аккаунта не существует"
+            }
+
+        } else if (requestCode == Constance.REQUEST_CODE_SIGN_UP){
+
+            login = data?.getStringExtra(Constance.LOGIN)!!
+            password = data.getStringExtra(Constance.PASSWORD)!!
+            name = data.getStringExtra(Constance.NAME)!!
+            lastName = data.getStringExtra(Constance.LAST_NAME)!!
+            secondName = data.getStringExtra(Constance.SECOND_NAME)!!
+            avatarImageId = data.getIntExtra(Constance.AVATAR_ID, 0)
+            bindingClass.imAvatar.visibility = View.VISIBLE
+            bindingClass.imAvatar.setImageResource(avatarImageId)
+            val textInfo = "$name $lastName $secondName"
+            bindingClass.tvInfo.text = textInfo
+            bindingClass.bHide.visibility = View.GONE
+            bindingClass.bExit.text = "Выйти"
+
         }
     }
 
-    fun onClickGoTest1(view: View){
+    fun onClickSignIn(view: View){
 
-        val i = Intent(this, TestActivity1::class.java)
-        i.putExtra("key", "What's your name?")
-        startActivityForResult(i, 100)
+        if (bindingClass.imAvatar.isVisible && bindingClass.tvInfo.text.toString() != "Такого аккаунта не существует"){
+            bindingClass.imAvatar.visibility = View.INVISIBLE
+            bindingClass.tvInfo.text = ""
+            bindingClass.bHide.visibility = View.VISIBLE
+            bindingClass.bExit.text = getString(R.string.sign_in)
+
+        } else {
+            val intent = Intent(this, SignInUpActivity::class.java)
+            intent.putExtra(Constance.SIGN_STATE, Constance.SIGN_IN_STATE)
+            startActivityForResult(intent, Constance.REQUEST_CODE_SIGN_IN)
+        }
+
+    }
+
+    fun onClickSignUp(view: View){
+
+        val intent = Intent(this, SignInUpActivity::class.java)
+        intent.putExtra(Constance.SIGN_STATE, Constance.SIGN_UP_STATE)
+        startActivityForResult(intent, Constance.REQUEST_CODE_SIGN_UP)
+
     }
 }
--- app/src/main/java/com/example/test_app/constance/Constance.kt
+++ app/src/main/java/com/example/test_app/constance/Constance.kt
@@ -1,15 +1,16 @@
 package com.example.test_app.constance
 
 object Constance {
-    const val DIRECTOR_SALARY = 100000
-    const val ENGINEER_SALARY = 50000
-    const val CLEANER_SALARY = 10000
+    const val SIGN_IN_STATE = "sign_in"
+    const val SIGN_UP_STATE = "sign_up"
+    const val SIGN_STATE = "sign_state"
+    const val REQUEST_CODE_SIGN_IN = 1212
+    const val REQUEST_CODE_SIGN_UP = 2121
 
-    const val DIRECTOR_PASSWORD = "4565"
-    const val ENGINEER_PASSWORD = "1235"
-    const val CLEANER_PASSWORD = "7766"
-
-    const val DIRECTOR = "Egor"
-    const val ENGINEER = "Andrey"
-    const val CLEANER = "Sergey"
+    const val LOGIN = "login"
+    const val PASSWORD = "password"
+    const val NAME = "name"
+    const val LAST_NAME = "last_name"
+    const val SECOND_NAME = "second_name"
+    const val AVATAR_ID = "avatar_id"
 }
